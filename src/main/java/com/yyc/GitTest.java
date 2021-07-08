5609e1be410444123ff15b9fca70b4d51e6e724c
yyc <1607941875@qq.com>
2021-07-08 16:32:53 +0800

    分支第2次提交

--- src/main/java/com/yyc/GitTest.java
+++ src/main/java/com/yyc/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hot-fix分支修改第二行");
+        System.out.println("分支修改第三行");
     }
 }
